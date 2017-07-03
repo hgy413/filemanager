d3075ae82b3d4162a144785e5cc47b2bbffe6271
nieyh <813825509@qq.com>
2017-07-03 10:19:17 +0800

    add usb tip

---
+++ app/src/main/java/com/jb/filemanager/function/usbstatemanager/UsbStateManager.java
@@ -0,0 +1,128 @@
+package com.jb.filemanager.function.usbstatemanager;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Toast;
+
+import com.jb.filemanager.Const;
+import com.jb.filemanager.TheApplication;
+import com.jb.filemanager.manager.spm.IPreferencesIds;
+import com.jb.filemanager.manager.spm.SharedPreferencesManager;
+import com.jb.filemanager.util.Logger;
+
+/**
+ * Created by nieyh on 17-6-30.
+ * Usb状态监听
+ */
+
+public class UsbStateManager {
+
+    private final String TAG = "UsbStateManager";
+
+    private static UsbStateManager sInstance;
+
+    private UsbStateManager() {}
+
+    public UsbStateManager getInstance() {
+        if (sInstance == null) {
+            sInstance = new UsbStateManager();
+        }
+        return sInstance;
+    }
+
+    //监听应用安装
+    BroadcastReceiver mUsbStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Bundle bundle = intent.getExtras();
+            //粘性广播 会持续接收到广告
+            if (bundle != null) {
+                boolean isConnected = bundle.getBoolean("connected");
+                boolean isHostConnected = bundle.getBoolean("host_connected");
+                boolean isAudioSource = bundle.getBoolean("audio_source");
+                boolean isAdb = bundle.getBoolean("adb");
+                boolean isMidi = bundle.getBoolean("midi");
+                Toast.makeText(context, "是否已经连接 >> " + isConnected, Toast.LENGTH_LONG).show();
+                Toast.makeText(context, "是否作为主机连接 >> " + isHostConnected, Toast.LENGTH_LONG).show();
+                Toast.makeText(context, "是否adb开启 >> " + isAdb, Toast.LENGTH_LONG).show();
+                Toast.makeText(context, "是否媒体资源开启 >> " + isAudioSource, Toast.LENGTH_LONG).show();
+                Toast.makeText(context, "是否midi >> " + isMidi, Toast.LENGTH_LONG).show();
+                if (isConnected && !isHostConnected) {
+                    //当USB连接上 并且 设备不是充当主设备 (也就是代表着usb连接电脑设备)
+                    tryShowTipWindow();
+                }
+            }
+        }
+    };
+
+    /**
+     * 尝试展示提示对话框
+     * */
+    private void tryShowTipWindow() {
+        if (!UsbStateTipDialog.isWindowIsTip) {
+            UsbStateTipDialog.show();
+        }
+    }
+
+    /**
+     * 查看开关是否开启
+     * @return 开启状态
+     */
+    public boolean isSwitchEnable() {
+        return SharedPreferencesManager.getInstance(TheApplication.getAppContext()).getBoolean(IPreferencesIds.KEY_USB_CONNECTED_TIP_ENABLE, true);
+    }
+
+    /**
+     * 改变功能开关 详情：如果之前功能是关闭的，则执行则将开启功能.
+     * @return 切换后的状态
+     */
+    public boolean changerSwitch() {
+        SharedPreferencesManager sharedPreferencesManager = SharedPreferencesManager.getInstance(TheApplication.getAppContext());
+        boolean isEnable = sharedPreferencesManager.getBoolean(IPreferencesIds.KEY_USB_CONNECTED_TIP_ENABLE, true);
+        isEnable = !isEnable;
+        if (isEnable) {
+            Logger.w(TAG, "开启USB监听开关");
+            // 开启功能
+            startMonitor();
+        } else {
+            Logger.w(TAG, "关闭USB监听开关");
+            // 关闭功能
+            stopMonitor();
+        }
+        sharedPreferencesManager.commitBoolean(IPreferencesIds.KEY_USB_CONNECTED_TIP_ENABLE, isEnable);
+        return isEnable;
+    }
+
+    /**
+     * 准备
+     * */
+    public void ready() {
+        if(isSwitchEnable()) {
+            startMonitor();
+        }
+    }
+
+    /**
+     * 注册usb状态监听器
+     * */
+    private void startMonitor() {
+        try {
+            IntentFilter intentFilter = new IntentFilter();
+            intentFilter.addAction("android.hardware.usb.action.USB_STATE");
+            TheApplication.getAppContext().registerReceiver(mUsbStateReceiver, intentFilter);
+        } catch (Exception e) {}
+    }
+
+    /**
+     * 停止usb状态监听
+     * */
+    private void stopMonitor() {
+        try {
+            TheApplication.getAppContext().unregisterReceiver(mUsbStateReceiver);
+        } catch (Exception e) {}
+    }
+}
