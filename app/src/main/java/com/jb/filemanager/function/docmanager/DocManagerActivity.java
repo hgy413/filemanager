7cbad524ec32ff8e7dec463e1ada5d9cc5759e9b
liqifa <liqf@m15.cn>
2017-07-04 10:28:02 +0800

    添加文档

---
+++ app/src/main/java/com/jb/filemanager/function/docmanager/DocManagerActivity.java
@@ -0,0 +1,15 @@
+package com.jb.filemanager.function.docmanager;
+
+import android.os.Bundle;
+
+import com.jb.filemanager.BaseActivity;
+import com.jb.filemanager.R;
+
+public class DocManagerActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doc_manager);
+    }
+}
