829fe51d780a5f955e8608d67e034b081a02eb64
wangzhaoqi <wangzhaoqi0209@live.cn>
2017-07-11 20:07:22 +0800

    单文件详情

--- app/src/main/java/com/jb/filemanager/ui/dialog/SingleFileDetailDialog.java
+++ app/src/main/java/com/jb/filemanager/ui/dialog/SingleFileDetailDialog.java
@@ -16,39 +16,77 @@ import java.io.File;
  *
  */
 
-public class SingleFileDetailDialog extends ScreenWidthDialog {
+public class SingleFileDetailDialog extends FMBaseDialog {
 
     public SingleFileDetailDialog(Activity act, File file, final Listener listener) {
         super(act, true);
         if (file != null && file.exists()) {
             View dialogView = View.inflate(act, R.layout.dialog_main_single_file_detail, null);
-            TextView okButton = (TextView) dialogView.findViewById(R.id.tv_main_single_file_detail_confirm);
-            TextView name = (TextView) dialogView.findViewById(R.id.tv_main_single_file_detail_name_value);
-            TextView location = (TextView) dialogView.findViewById(R.id.tv_main_single_file_detail_location_value);
-            TextView modifyTime = (TextView) dialogView.findViewById(R.id.tv_main_single_file_detail_modify_time_value);
-            TextView size = (TextView) dialogView.findViewById(R.id.tv_main_single_file_detail_size_value);
-            TextView containTitle = (TextView) dialogView.findViewById(R.id.tv_main_single_file_detail_contain_title);
-            TextView containValue = (TextView) dialogView.findViewById(R.id.tv_main_single_file_detail_contain_value);
 
-            name.setText(file.getName());
-            location.setText(file.getAbsolutePath());
-            modifyTime.setText(TimeUtil.getTime(file.lastModified()));
-            size.setText(ConvertUtils.getReadableSize(file.length()));
-
-            if (file.isDirectory()) {
-                int[] counts = FileUtil.countFolderAndFile(file);
-                containValue.setText(act.getString(R.string.main_dialog_single_detail_contain, counts[0], counts[1]));
-            } else {
-                containTitle.setVisibility(View.GONE);
-                containValue.setVisibility(View.GONE);
+            TextView title = (TextView) dialogView.findViewById(R.id.tv_main_single_file_detail_title);
+            if (title != null) {
+                title.getPaint().setAntiAlias(true);
+                if (file.isDirectory()) {
+                    title.setText(R.string.dialog_single_file_detail_title_folder);
+                } else {
+                    title.setText(R.string.dialog_single_file_detail_title_file);
+                }
             }
 
-            okButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    listener.onConfirm(SingleFileDetailDialog.this);
+            TextView name = (TextView) dialogView.findViewById(R.id.tv_main_single_file_detail_name_value);
+            if (name != null) {
+                name.getPaint().setAntiAlias(true);
+                name.setText(file.getName());
+            }
+
+            TextView location = (TextView) dialogView.findViewById(R.id.tv_main_single_file_detail_location_value);
+            if (location != null) {
+                location.getPaint().setAntiAlias(true);
+                location.setText(file.getAbsolutePath());
+            }
+
+            TextView modifyTime = (TextView) dialogView.findViewById(R.id.tv_main_single_file_detail_modify_time_value);
+            if (modifyTime != null) {
+                modifyTime.getPaint().setAntiAlias(true);
+                modifyTime.setText(TimeUtil.getTime(file.lastModified()));
+            }
+
+            TextView size = (TextView) dialogView.findViewById(R.id.tv_main_single_file_detail_size_value);
+            if (size != null) {
+                size.getPaint().setAntiAlias(true);
+                size.setText(ConvertUtils.getReadableSize(file.length()));
+            }
+
+            TextView containTitle = (TextView) dialogView.findViewById(R.id.tv_main_single_file_detail_contain_title);
+            if (containTitle != null) {
+                containTitle.getPaint().setAntiAlias(true);
+                if (!file.isDirectory()) {
+                    containTitle.setVisibility(View.GONE);
                 }
-            }); // 确定按钮点击事件
+            }
+
+            TextView containValue = (TextView) dialogView.findViewById(R.id.tv_main_single_file_detail_contain_value);
+            if (containValue != null) {
+                containValue.getPaint().setAntiAlias(true);
+                if (file.isDirectory()) {
+                    int[] counts = FileUtil.countFolderAndFile(file);
+                    containValue.setText(act.getString(R.string.main_dialog_single_detail_contain, counts[0], counts[1]));
+                } else {
+                    containValue.setVisibility(View.GONE);
+                }
+            }
+
+            TextView okButton = (TextView) dialogView.findViewById(R.id.tv_main_single_file_detail_confirm);
+            if (okButton != null) {
+                okButton.getPaint().setAntiAlias(true);
+
+                okButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        listener.onConfirm(SingleFileDetailDialog.this);
+                    }
+                }); // 确定按钮点击事件
+            }
 
             setContentView(dialogView);
         }
