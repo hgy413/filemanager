23be823b774036807bb5f6f46f96d992c97416f7
wangzhaoqi <wangzhaoqi0209@live.cn>
2017-07-25 15:51:16 +0800

    换图，增加统计

--- app/src/main/java/com/jb/filemanager/statistics/StatisticsConstants.java
+++ app/src/main/java/com/jb/filemanager/statistics/StatisticsConstants.java
@@ -8,7 +8,7 @@ package com.jb.filemanager.statistics;
 public class StatisticsConstants {
 
     //101
-    public static final int LOG_ID_758 = 758;
+    public static final int LOG_ID_758 = 862;
     //102
     public static final int LOG_ID_759 = 759;
     //103 功能id
@@ -24,6 +24,77 @@ public class StatisticsConstants {
     // 侧边栏呼出
     public static final String HOME_SIDE_SHOW = "f000_drawer";
 
-    //侧边栏关闭
+    // 侧边栏关闭
     public static final String HOME_SIDE_CLOSE = "t000_drawer_close";
+
+    // Home页
+    public static final String HOME_CLICK_DRAWER            = "c000_home_tool";
+    public static final String HOME_CLICK_SEARCH            = "c000_home_search";
+    public static final String HOME_CLICK_TAB_CATEGORY      = "c000_home_cate";
+    public static final String HOME_CLICK_TAB_STORAGE       = "c000_home_stor";
+    public static final String HOME_CLICK_CATEGORY_PHOTO    = "c000_home_pho";
+    public static final String HOME_CLICK_CATEGORY_VIDEO    = "c000_home_vedio";
+    public static final String HOME_CLICK_CATEGORY_APP      = "c000_home_app";
+    public static final String HOME_CLICK_CATEGORY_AUDIO    = "c000_home_audio";
+    public static final String HOME_CLICK_CATEGORY_DOC      = "c000_home_doc";
+    public static final String HOME_CLICK_CATEGORY_ZIP      = "c000_home_zip";
+    public static final String HOME_CLICK_CATEGORY_DOWNLOAD = "c000_home_down";
+    public static final String HOME_CLICK_CATEGORY_RECENT   = "c000_home_recent";
+    public static final String HOME_CLICK_CATEGORY_AD       = "c000_home_ad";
+    public static final String HOME_CLICK_SWITCH_SD         = "c000_home_sd";
+    public static final String HOME_CLICK_CLEAN             = "c000_home_clean";
+
+    // 搜索页
+    public static final String SEARCH_EXIT_SEARCH           = "c000_home_exit";
+    public static final String SEARCH_SHOW_SEARCH_ANIM      = "f000_home_search";
+    public static final String SEARCH_SHOW_SEARCH_RESULT    = "f000_home_result";
+    public static final String SEARCH_CLICK_SEARCH_RESULT   = "c000_home_result";
+
+    // 工具栏
+    public static final String DRAWER_ENTER_APP_LOCKER      = "c000_tool_locker";
+    public static final String DRAWER_ENTER_SMART_CHARGE    = "c000_tool_charge";
+    public static final String DRAWER_CLICK_USB_SWITCH      = "c000_tool_usb";
+    public static final String DRAWER_CLICK_LOW_SWITCH      = "c000_tool_low";
+    public static final String DRAWER_CLICK_LOGGER_SWITCH   = "c000_tool_logger";
+    public static final String DRAWER_CLICK_RATING          = "c000_tool_rating";
+    public static final String DRAWER_CLICK_UPDATE          = "c000_tool_update";
+    public static final String DRAWER_CLICK_FEEDBACK        = "c000_tool_Feed";
+    public static final String DRAWER_CLICK_ABOUT           = "c000_tool_About";
+
+    public static final String STORAGE_CLICK_PATH           = "c000_stor_way";
+    public static final String STORAGE_CLICK_STYLE_SWITCH   = "c000_stor_list";
+    public static final String STORAGE_CLICK_SEARCH         = "c000_stor_search";
+    public static final String STORAGE_CLICK_ACTION_MORE    = "c000_stor_caidan";
+    public static final String STORAGE_CLICK_CREATE_FOLDER  = "c000_stor_creat";
+    public static final String STORAGE_CREATE_FOLDER_OK     = "c000_stor_newok";
+    public static final String STORAGE_CREATE_FOLDER_CANCEL = "c000_stor_newexit";
+    public static final String STORAGE_CLICK_SORT           = "c000_stor_sort";
+    public static final String STORAGE_SORT_NAME            = "c000_stor_name";
+    public static final String STORAGE_SORT_DATE            = "c000_stor_data";
+    public static final String STORAGE_SORT_TYPE            = "c000_stor_type";
+    public static final String STORAGE_SORT_SIZE            = "c000_stor_size";
+    public static final String STORAGE_SORT_ASCENDING       = "c000_stor_Ascending";
+    public static final String STORAGE_SORT_DESCENDING      = "c000_stor_Descending";
+    public static final String STORAGE_CLICK_CUT            = "c000_stor_cut";
+    public static final String STORAGE_CLICK_COPY           = "c000_stor_copy";
+    public static final String STORAGE_CLICK_PASTE          = "c000_stor_paste";
+    public static final String STORAGE_CLICK_DELETE         = "c000_stor_delete";
+    public static final String STORAGE_CLICK_BOTTOM_MORE    = "c000_stor_more";
+    public static final String STORAGE_CLICK_DETAILS        = "c000_stor_detail";
+    public static final String STORAGE_CLICK_RENAME         = "c000_stor_rename";
+
+    // TODO @nieyh 统计 USB 加完清除todo
+    public static final String LOW_SHOW                     = "f000_tool_low";
+    public static final String LOW_CLICK_CLEAN              = "c000_tool_clean";
+
+    // TODO 统计logger
+    public static final String LOGGER_SHOW                  = "f000_tool_logger";
+
+    // TODO @nieyh 统计 USB 加完清除todo
+    public static final String USB_SHOW                     = "f000_tool_usb";
+    public static final String USB_CLICK_MANAGER            = "c000_tool_manage";
+
+    // TODO @liqf 统计 清理按钮动画展示 加完清除todo
+    public static final String HOME_SHOW_CLEAN_ANIM         = "f000_home_clean";
+
 }
